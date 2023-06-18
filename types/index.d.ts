776dd0ea2920ca99ae9e08040785c065843d852a
Frankie <26727138+frankievalentine@users.noreply.github.com>
2023-06-18 11:55:53 -0700

    Remove components to build prod

--- types/index.d.ts
+++ types/index.d.ts
@@ -9,6 +9,30 @@ export type SiteConfig = {
   }
 }
 
+export type NavItem = {
+  title: string
+  href: string
+  disabled?: boolean
+}
+
+export type MainNavItem = NavItem
+
+export type SidebarNavItem = {
+  title: string
+  disabled?: boolean
+  external?: boolean
+  icon?: keyof typeof Icons
+} & (
+  | {
+      href: string
+      items?: never
+    }
+  | {
+      href?: string
+      items: NavLink[]
+    }
+)
+
 export type MarketingConfig = {
   mainNav: MainNavItem[]
 }
