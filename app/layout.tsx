69b4d6f4618004551b269ed8dabc380d9f22b5b0
Frankie <26727138+frankievalentine@users.noreply.github.com>
2023-06-18 23:33:36 -0700

    Fix font name

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const fontSans = FontSans({
 
 // Font files can be colocated inside of `pages`
 const fontHeading = localFont({
-  src: '../assets/fonts/ClashDisplay-SemiBold.woff2',
+  src: '../assets/fonts/ClashDisplay-Semibold.woff2',
   variable: '--font-heading',
 })
 
