398ac674c711ebfa799b2703d05475ce24468e29
Frankie <26727138+frankievalentine@users.noreply.github.com>
2023-08-30 14:31:02 -0700

    update next config CORS

--- next.config.js
+++ next.config.js
@@ -21,6 +21,27 @@ const nextConfig = {
   //         },
   //     ];
   // },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "v3digital.studio" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ]
+  },
   reactStrictMode: true,
   swcMinify: true,
   webpack(config) {
